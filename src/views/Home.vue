6db505daf39bfbdba46a12b8669cf27399a1c2f4
WangShaoBin <wangshaobin@arrail-dental.com>
2019-07-01 18:01:48 +0800

    vue初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <el-container>
+        <div>
+            <el-button @click="show = !show">Click Me</el-button>
+
+            <div style="display: flex; margin-top: 20px; height: 100px;">
+                <transition name="el-fade-in-linear">
+                    <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+                </transition>
+                <transition name="el-fade-in">
+                    <div v-show="show" class="transition-box">.el-fade-in</div>
+                </transition>
+            </div>
+            <router-link to='/hello'>
+                <button>点我到第二个页面</button>
+            </router-link>
+            <el-button type="primary" @click="f()">点击我跳转</el-button>
+        </div>
+    </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data: () => ({
+            show: true
+        })
+        , methods: {
+            f: function () {
+                this.$router.push({path: '/hello'})
+            }
+        }
+    }
 </script>
+
+<style>
+    .transition-box {
+        margin-bottom: 10px;
+        width: 200px;
+        height: 100px;
+        border-radius: 4px;
+        background-color: #409EFF;
+        text-align: center;
+        color: #fff;
+        padding: 40px 20px;
+        box-sizing: border-box;
+        margin-right: 20px;
+    }
+</style>
+
